f10ded68eb1dfb08dc2b07b57eebe1ef2747ba4f
CJE-cpu <chlwjddms7309@gmail.com>
2023-04-03 21:13:28 +0900

    final commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         <li className='p__opensans'><a href="#about">About</a></li>
         <li className='p__opensans'><a href="#menu">Menu</a></li>
         <li className='p__opensans'><a href="#awards">Awards</a></li>
-        <li className='p__opensans'><a href="#Contact">Contact</a></li>
+        <li className='p__opensans'><a href="#contact">Contact</a></li>
       </ul>
       <div className='app__navbar-login'>
         <a href="#login" className='p__opensans'>Log In / Register</a>
@@ -32,12 +32,12 @@ const Navbar = () => {
         {toggleMenu && (
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
           <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
-          <ul className='app__navbar-smallscreen-links'>
+          <ul className='app__navbar-smallscreen_links'>
             <li className='p__opensans'><a href="#home">Home</a></li>
             <li className='p__opensans'><a href="#about">About</a></li>
             <li className='p__opensans'><a href="#menu">Menu</a></li>
             <li className='p__opensans'><a href="#awards">Awards</a></li>
-            <li className='p__opensans'><a href="#Contact">Contact</a></li>
+            <li className='p__opensans'><a href="#contact">Contact</a></li>
           </ul>
         </div>
         )}
